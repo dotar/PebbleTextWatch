0762de9cf7cbbb81cf07672666eeed2ef793ba16
dotar <torsten.augustsson@gmail.com>
2013-04-17 04:23:20 +0300

    Update TextWatch.c
    Get menu icon from resource

--- src/TextWatch.c
+++ src/TextWatch.c
@@ -9,9 +9,10 @@
 
 #define MY_UUID { 0x49, 0x6E, 0x04, 0xAD, 0x13, 0x2A, 0x48, 0xAB, 0xB1, 0x65, 0x7F, 0xF4, 0xA9, 0x98, 0x72, 0xD2 }
 PBL_APP_INFO(MY_UUID,
-             "TextWatch Swe", "Wip Interactive/dotar",
+             "TextWatchSwe", "Wip Interactive/dotar",
              1, 0,
-             DEFAULT_MENU_ICON,
+             #DEFAULT_MENU_ICON,
+             RESOURCE_ID_IMAGE_MENU_ICON,
 #if DEBUG
              APP_INFO_STANDARD_APP
 #else
